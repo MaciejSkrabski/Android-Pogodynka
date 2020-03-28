717b9e2a0246293c30f4e7ac1c94c3576a229b18
Maciej Skrabski <maciej.skrabski@gmail.com>
2020-03-28 21:59:47 +0100

    It's working!

---
+++ app/src/main/java/com/example/pogodynka/FragmentWeather.kt
@@ -0,0 +1,117 @@
+package com.example.pogodynka
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_weather.*
+import kotlinx.android.synthetic.main.fragment_weather.view.*
+import java.net.URL
+import kotlin.concurrent.thread
+import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class FragmentWeather : Fragment() {
+    val CITY: String = "gliwice,pl"
+    val API: String = "c73352e3d33a074760e095d142f76ce4"
+    var response:String? = null
+    var successful:Boolean = true
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+
+        // Snippet from "Navigate to the next Fragment" section goes here.
+
+        val view = inflater.inflate(R.layout.fragment_weather, container, false)
+        view.next_button.setOnClickListener { getWeather(city_input.text.toString()) }
+        view.clear_button.setOnClickListener { city_input.text = null }
+
+
+        return view
+    }
+
+    // http://openweathermap.org/img/wn/10d@2x.png
+    // http://openweathermap.org/img/wn/$ICON@2x.png
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getWeather(CITY)
+
+    }
+
+
+    fun getWeather(city: String?){
+        try {
+            callApi(city)
+            if (successful) {
+                val gson = Gson()
+                val parsed = gson.fromJson(response, DataResponse::class.java)
+                Log.d("parsed", parsed.toString())
+                Picasso.get().load("https://openweathermap.org/img/wn/${parsed.weather[0].icon}@2x.png").into(image)
+
+                city_name.text = parsed.name
+                temperature.text = (parsed.main.temp-273.15).toInt().toString() + "ºC"
+                pressure.text = parsed.main.pressure.toString()
+                desc.text = parsed.weather[0].description
+                sunrise.text = dateFormatter(parsed.sys.sunrise.toLong())
+                sunset.text = dateFormatter(parsed.sys.sunset.toLong())
+                datetime.text = dateFormatter(parsed.dt.toLong())
+            }
+        } catch (e: Error) {
+            Toast.makeText(activity, e.message, Toast.LENGTH_SHORT)
+        }
+    }
+
+    fun callApi(city: String?){
+
+        try {
+            thread(start = true) {
+                Log.d("Thread","${Thread.currentThread()} has run.")
+                try {
+                    response =
+                        URL("https://api.openweathermap.org/data/2.5/weather?q=$city&APPID=$API").readText(
+                            Charsets.UTF_8
+                        )
+                }
+                catch(e: Error){
+                    successful = false
+                    Log.d("Connection error", e.message)
+                    Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
+                }
+                Log.d("resp", response)
+
+
+            }.join()
+        }
+        catch (e: Exception) {
+            Log.d("error", e.toString())
+            response = null
+        }
+    }
+
+
+    fun dateFormatter(epoch: Long): String {
+        // epoch = 1557954848
+        val date = Date(epoch * 1000L)
+        val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm")
+        return sdf.format(date)
+        // returned value = "2019-05-15 14:14:08.000000"
+    }
+}
--- app/src/main/java/com/example/pogodynka/MainActivity.kt
+++ app/src/main/java/com/example/pogodynka/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.pogodynka
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
 import kotlin.concurrent.thread
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,33 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val CITY: String = "gliwice,pl"
-        val API: String = "c73352e3d33a074760e095d142f76ce4"
-
-        var response:String?
-        try {
-
-            Log.d("response test", "haba before")
-            thread(start = true) {
-                Log.d("Thread","${Thread.currentThread()} has run.")
-                response = URL("https://api.openweathermap.org/data/2.5/weather?q=gliwice,pl&APPID=c73352e3d33a074760e095d142f76ce4").readText(Charsets.UTF_8)
-                runOnUiThread {testing.text=response}
-                Log.d("resp", response)
-
-            }
-
-
-
-            //Log.d("resp", response)
-            Log.d("response test", "haba after")
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                //.add(R.id.container, LoginFragment())
+                .add(R.id.container, FragmentWeather()) // omit LoginFragment for testing
+                .commit()
         }
-        catch (e: Exception) {
-            Log.d("error", e.toString())
-            response = null
-        }
+
+        //callApi(CITY)
+
         //testing.text = response
 
     }
+
+
 
 
 }
